820329c6f466da3ca6647d6e7fe40e88df8aafc0
Lindy Briggs <briggs.lindy@yahoo.com>
2022-05-23 13:50:06 -0500

    Thoughts model updated/added

---
+++ models/Thoughts.js
@@ -0,0 +1,67 @@
+const { Schema, model, Types } = require('mongoose');
+const moment = require('moment');
+
+const thoughtsSchema = new Schema(
+    {
+    thoughtText: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 280
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (date) => moment(date).format('MMMM Do YYYY, h:mm:ss a')
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    reactions: [reactionSchema]
+    },
+    {
+    toJSON: {
+        virtuals: true,
+        getters: true
+    },
+    id: false
+    }
+);
+
+const reactionSchema = new Schema(
+    {
+    // Set custom ID 
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        maxlength: 280
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (date) => moment(date).format('MMMM Do YYYY, h:mm:ss a')
+    }
+    },
+    {
+    toJSON: {
+        getters: true
+    } 
+    }
+);
+
+thoughtsSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+});
+
+const Thoughts = model('Thoughts', thoughtsSchema);
+
+module.exports = Thoughts;
