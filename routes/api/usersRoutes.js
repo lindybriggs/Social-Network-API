c0d43ad13bb53ea43558f2890a208afd2292e0aa
Lindy Briggs <briggs.lindy@yahoo.com>
2022-05-23 15:31:33 -0500

    usersRoutes updated/added

---
+++ routes/api/usersRoutes.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+const {
+  getUsers,
+  getSingleUser,
+  createUser,
+  deleteUser,
+  updateUser,
+} = require('../../controllers/usersController');
+
+// /api/users
+router.route('/')
+  .get(getUsers)
+  .post(createUser);
+
+// /api/users/:id
+router.route('/:id')
+  .get(getSingleUser)
+  .put(updateUser)
+  .delete(deleteUser);
+
+module.exports = router;
