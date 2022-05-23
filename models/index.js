9f7c90552802a52ecd3f20dccb18c02f79c6caf1
Lindy Briggs <briggs.lindy@yahoo.com>
2022-05-23 14:49:07 -0500

    usersController updated with methods

--- models/index.js
+++ models/index.js
@@ -1,4 +1,4 @@
-const Student = require('./Student');
-const Course = require('./Course');
+const Users = require('./Users');
+const Thoughts = require('./Thoughts');
 
-module.exports = { Student, Course };
+module.exports = { Users, Thoughts };
